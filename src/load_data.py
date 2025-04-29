d4f5c77c4c796d44389903d0db2d92c63c7a940e
MaierElias <me7770@mci4me.at>
2025-04-29 15:10:19 +0200

    Added Julius to pyproject.toml

--- src/load_data.py
+++ src/load_data.py
@@ -1,5 +1,5 @@
 import numpy as np
-from src.sort import bubble_sort
+from sort import bubble_sort
 import matplotlib.pyplot as plt
 
 def load_data(file_path):
@@ -23,7 +23,7 @@ def load_data(file_path):
 
 
 if __name__ == "__main__":
-    data = load_data('activity.csv')
+    data = load_data('data/activity.csv')
     power_W = data['PowerOriginal']
     print(power_W)
     sorted_power_W = bubble_sort(power_W)
