86459e14f94dfb2ffb88d38d2aa4d428958b1bf3
juliusmayr <mj4127@mci4me.at>
2025-05-27 14:30:14 +0200

    Streamlit_edit_1

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ import streamlit as st
 st.write("# Hello, Streamlit!")
 
 st.write("## Zweite Überschrift")
+
 st.write ("This is a simple Streamlit app to demonstrate the setup")
+
+st.selectbox("Wähle eine Versuchsperson", options=["Name 1", "Name 2", "Name 3"])
+
+st.button("Klicke mich!")
+
