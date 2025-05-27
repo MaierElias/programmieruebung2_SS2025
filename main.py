bf7ebe1505d932c6fd58dec0f6e9336973f1947b
MaierElias <me7770@mci4me.at>
2025-05-27 15:57:51 +0200

    implemented connected pictures of selected persons in streamlit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+from PIL import Image
+
+from src.read_data import get_person_names, get_person_data_by_name, get_person_image_by_name
+
+
 if "selected_person" not in st.session_state:
     st.session_state.selected_person = "Name 1"
 
@@ -9,6 +14,11 @@ st.write("## Zweite Überschrift")
 
 st.write ("This is a simple Streamlit app to demonstrate the setup")
 
-st.session_state.selected_person = st.selectbox("Wähle eine Versuchsperson", options=["Name 1", "Name 2", "Name 3"])
+st.session_state.selected_person = st.selectbox("Wähle eine Versuchsperson", options = get_person_names())
 
 st.write(st.session_state.selected_person)
+
+# Laden eines Bilds
+image = Image.open(get_person_image_by_name(st.session_state.selected_person))
+# Anzeigen eines Bilds mit Caption
+st.image(image, caption=st.session_state.selected_person)
--- src/read_data.py
+++ src/read_data.py
@@ -1,9 +1,54 @@
 import json
 
-# Opening JSON file
-file = open("data/person_db.json")
+def get_person_data():
+    """
+    Returns the person data loaded from the JSON file.
+    """
+    
+    # Opening JSON file
+    file = open("data/person_db.json")
 
-# Loading the JSON File in a dictionary
-person_data = json.load(file)
+    # Loading the JSON File in a dictionary
+    person_data = json.load(file)
 
-print(person_data)
+    return person_data
+
+
+def get_person_names():
+    """
+    Returns a list of person names from the loaded person data.
+    """
+    # gib mir die Liste mit allen Personen
+    persons_dict = get_person_data()
+    names_list = []
+    # gehe durch die Liste
+    for person_data in persons_dict:
+        # jeder Eintrag ist ein Dictionary mit den Feldern (firstname, lastname)
+        names_list.append(person_data["lastname"] + ", " + person_data["firstname"])
+        # hänge das an die Namensliste an
+    return names_list
+
+def get_person_data_by_name(personname):
+    all_persons = get_person_data()
+    lastname, firstname = personname.split(", ")
+    
+    for person_data in all_persons:
+        if person_data["firstname"] == firstname and person_data["lastname"] == lastname:
+            return person_data # dictionary einer Person
+
+
+def get_person_image_by_name(personname):
+    picture_path = get_person_data_by_name(personname)["picture_path"]
+    return picture_path  # gibt den Pfad zum Bild zurück
+
+if __name__ == "__main__":
+    # Example usage
+    person_data = get_person_data()
+    print(person_data)
+    person_names = get_person_names()
+    print(person_names)
+    person_name = "Huber, Julian"
+    person_dict = get_person_data_by_name(person_name)
+    print(person_dict)
+    get_person_image_by_name(person_name)
+    print(get_person_image_by_name(person_name))
