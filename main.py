040ff26cb738091f58f145d9c6774c76fa2a8a9a
MaierElias <me7770@mci4me.at>
2025-05-27 14:49:55 +0200

    added read_data.py

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import streamlit as st
 
+if "selected_person" not in st.session_state:
+    st.session_state.selected_person = "Name 1"
+
 st.write("# Hello, Streamlit!")
 
 st.write("## Zweite Überschrift")
 
 st.write ("This is a simple Streamlit app to demonstrate the setup")
 
-st.selectbox("Wähle eine Versuchsperson", options=["Name 1", "Name 2", "Name 3"])
+st.session_state.selected_person = st.selectbox("Wähle eine Versuchsperson", options=["Name 1", "Name 2", "Name 3"])
 
-st.button("Klicke mich!")
-
+st.write(st.session_state.selected_person)
