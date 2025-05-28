be7f820bcec7902c9c182c7332608b1c6b7c631d
MaierElias <me7770@mci4me.at>
2025-05-28 15:30:27 +0200

    added activity plot to streamlit, solved a problem that would stop streamlit from working

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import Image
 
 from src.read_data import get_person_names, get_person_data_by_name, get_person_image_by_name
 
+from src.analyze_activity_data import create_plot
 
 if "selected_person" not in st.session_state:
     st.session_state.selected_person = "Name 1"
@@ -19,6 +20,9 @@ st.session_state.selected_person = st.selectbox("Wähle eine Versuchsperson", op
 st.write(st.session_state.selected_person)
 
 # Laden eines Bilds
+selected_person_data = get_person_data_by_name(st.session_state.selected_person)
 image = Image.open(get_person_image_by_name(st.session_state.selected_person))
 # Anzeigen eines Bilds mit Caption
 st.image(image, caption=st.session_state.selected_person)
+
+st.plotly_chart(create_plot())
--- src/analyze_activity_data.py
+++ src/analyze_activity_data.py
@@ -2,7 +2,7 @@
 #%% Zelle 1
 import pandas as pd
 
-dataframe = pd.read_csv('../data/activities/activity.csv')
+dataframe = pd.read_csv('data/activities/activity.csv')
 dataframe
 # %%
 dataframe["PowerOriginal"].mean()
@@ -17,4 +17,54 @@ dataframe["PowerOriginal"] > 300
 dataframe["HighPower"] = dataframe["PowerOriginal"] > 300
 dataframe["HighPower"].sum()
 dataframe["HighPower"].value_counts()
+
+# %%
+dataframe["Zone"] = None
+
+hr_max = dataframe["HeartRate"].max()
+hr_max
+
+# %%
+untergrenzen_zonen = {}
+zone = 1
+for faktor in range(50, 100, 10):	
+    untergrenzen_zonen[f"Zone {zone}"] = float(hr_max * faktor/100)
+    # print("Zone", zone)
+    # print(hr_max * faktor)
+    zone += 1
+
+untergrenzen_zonen
+# %% Füge eine neue Spalte Zone hinzu, die die Zone basierend auf der Herzfrequenz angibt
+list_zone = []
+
+dataframe["Zone"] = None
+
+for index, row in dataframe.iterrows():
+    #print(row["HeartRate"])
+    current_hr = row["HeartRate"]
+    if current_hr >= untergrenzen_zonen["Zone 5"]:
+        list_zone.append("Zone 5")
+    elif current_hr >= untergrenzen_zonen["Zone 4"]:
+        list_zone.append("Zone 4")
+    elif current_hr >= untergrenzen_zonen["Zone 3"]:
+        list_zone.append("Zone 3")
+    elif current_hr >= untergrenzen_zonen["Zone 2"]:
+        list_zone.append("Zone 2")
+    elif current_hr >= untergrenzen_zonen["Zone 1"]:
+        list_zone.append("Zone 1")
+    else:
+        list_zone.append("Zone 0")
+
+dataframe["Zone"] = list_zone
+
+dataframe["Zone"].value_counts()
+# %%
+df_groups = dataframe.groupby("Zone").mean()
+df_groups[["PowerOriginal", "HeartRate"]]
+# %% erstellen eines interaktiven Plots
+import plotly.express as px
+def create_plot():
+    fig = px.line(dataframe[["PowerOriginal", "HeartRate"]], y = ["PowerOriginal", "HeartRate"])
+    fig.show()
+    return fig
 # %%
