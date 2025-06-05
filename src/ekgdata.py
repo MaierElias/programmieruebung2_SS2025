c5ca3529dc555b361bfd2494efc5ddabf9b799f0
MaierElias <me7770@mci4me.at>
2025-06-05 17:14:20 +0200

    added find_peaks and load_by_id to EKGdata

--- src/ekgdata.py
+++ src/ekgdata.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 
 # %% Objekt-Welt
-
+from person import Person
 # Klasse EKG-Data für Peakfinder, die uns ermöglicht peaks zu finden
 
 class EKGdata:
@@ -18,6 +18,36 @@ class EKGdata:
         self.df = pd.read_csv(self.data, sep='\t', header=None, names=['Messwerte in mV','Zeit in ms',])
         self.df = self.df.iloc[:5000]  # Entferne die erste Zeile, da sie nur die Spaltennamen enthält
 
+    @staticmethod
+    def load_by_id(input_persons, test_id):
+        """
+        Diese Funktion lädt den EKG-Test anhand der ID und der Personen-Datenbank.
+        """
+        input_persons = Person.load_person_data()
+        for person in input_persons:
+            for ekg_test in person["ekg_tests"]:
+                if ekg_test["id"] == test_id:
+                    return ekg_test
+    
+    def find_peaks(self, threshold=340):
+        """
+        a function that finds R-peaks in an EKG signal.
+        """
+
+        list_of_peaks = []	
+
+        for index, row in self.df.iterrows():   
+            if index == self.df.index.max():
+                break 
+
+            current_value = row['Messwerte in mV']
+
+            # ist der current_value größer als der Vorgänger und Nachfolger
+            if current_value > self.df.iloc[index-1]["Messwerte in mV"] and current_value >= self.df.iloc[index+1]["Messwerte in mV"]:
+                #print("Found a peak at index: ", index)
+                if current_value > threshold:
+                    list_of_peaks.append(index)
+        return list_of_peaks
 
     def plot_time_series(self):
 
