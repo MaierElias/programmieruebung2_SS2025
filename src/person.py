0d9e740af859a3ec3ff806c5672d064e1c6b2de6
juliusmayr <mj4127@mci4me.at>
2025-06-05 17:06:50 +0200

    added calc_age and calc_max_hr load_by_id

--- src/person.py
+++ src/person.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime, date
 
 class Person:
     
@@ -8,7 +9,17 @@ class Person:
         file = open("data/person_db.json")
         person_data = json.load(file)
         return person_data
+    
 
+    @staticmethod
+    def load_by_id(person_id):
+        """A Function that knows where te persons ID is returns a Dictionary with the Persons"""
+        person_data = Person.load_person_data()
+        for person in person_data: 
+            if person_id == person["id"]:
+                return person_data[person_id-1] 
+        return {'None'}
+            
     @staticmethod
     def get_person_list(person_data):
         """A Function that takes the persons-dictionary and returns a list auf all person names"""
@@ -41,6 +52,7 @@ class Person:
         else:
             return {}
         
+        
     def __init__(self, person_dict) -> None:
         self.date_of_birth = person_dict["date_of_birth"]
         self.firstname = person_dict["firstname"]
@@ -49,9 +61,27 @@ class Person:
         self.id = person_dict["id"]
         self.gender = person_dict["gender"]
 
+    def calc_age(self):
+        """A Function that calculates the age of a person based on the date of birth"""
+        #date_of_birth = datetime.strptime(self.date_of_birth, "%Y-%m-%d").date()
+        today = date.today()
+        age = today.year - int(self.date_of_birth)
+        return age
+
+    def calc_max_heart_rate(self):
+        """A Function that calculates the Persons maximum Heart Rate based on the age"""
+        age = self.calc_age()
+        max_heart_rate = 220 - age
+        return max_heart_rate
+        
+
 if __name__ == "__main__":
     print("This is a module with some functions to read the person data")
     persons = Person.load_person_data()
     person_names = Person.get_person_list(persons)
     print(person_names)
     print(Person.find_person_data_by_name("Huber, Julian"))
+    print(Person.load_by_id(1))
+    person = Person(Person.load_by_id(1))
+    print(person.calc_age())
+    print(person.calc_max_heart_rate())
